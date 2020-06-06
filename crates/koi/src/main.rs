c8126ec9e0d628cfec9fe30a9370868e71229c6f
tahscenery <tahscenery@gmail.com>
2020-06-07 02:10:20 +1000

    Parsing command line options

--- crates/koi/src/main.rs
+++ crates/koi/src/main.rs
@@ -1,5 +1,23 @@
+fn print_usage() {
+    println!("{}", include_str!("../usage.txt"));
+}
+
+fn print_version() {
+    if let Some(version) = option_env!("CARGO_PKG_VERSION") {
+        println!("koi {}", version);
+    } else {
+        eprintln!("Failed to get version.");
+    }
+}
+
 fn main() {
-    koi_build::say_hello();
-    koi_ide::say_hello();
-    koi_repl::say_hello();
+    let mut quiet_mode = false;
+    let mut args = std::env::args();
+
+    match (args.next(), args.next(), args.next()) {
+        (_, Some(ref opt), _) if opt == "-h" || opt == "--help" => print_usage(),
+        (_, Some(ref opt), _) if opt == "-q" || opt == "--quiet" => quiet_mode = true,
+        (_, Some(ref opt), _) if opt == "-V" || opt == "--version" => print_version(),
+        _ => print_usage()
+    }
 }
