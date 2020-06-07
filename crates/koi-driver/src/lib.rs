cc5c51eb8c5eb43c8f70a5ee3ae4bfef236e99b1
tahscenery <tahscenery@gmail.com>
2020-06-07 16:31:34 +1000

    koi-driver: Added struct to handle toolchain

--- crates/koi-driver/src/lib.rs
+++ crates/koi-driver/src/lib.rs
@@ -4,7 +4,28 @@ pub use diagnostic::Diagnostic;
 
 type Result<T> = std::result::Result<T, Vec<Diagnostic>>;
 
+#[allow(dead_code)]
+pub struct Driver<'a> {
+    file_name: &'a str,
+}
+
+impl<'a> Driver<'a> {
+    pub fn with(file_name: &'a str) -> Self {
+        Self { file_name }
+    }
+
+    pub fn start(self) -> Result<()> {
+        self.tokenize_source()?;
+        Ok(())
+    }
+
+    pub fn tokenize_source(self) -> Result<()> {
+        Err(vec![Diagnostic::new("E0001", "Error message")])
+    }
+}
+
 pub fn start(file_name: &str) -> Result<()> {
-    println!("file_name: {}", file_name);
-    Err(vec![Diagnostic::new("E0001", "Error message")])
+    // println!("file_name: {}", file_name);
+    let driver = Driver::with(file_name);
+    driver.start()
 }
