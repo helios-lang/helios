fbc329fc646ea5ec86a12abd35978333ad7d24a3
tahscenery <tahscenery@gmail.com>
2020-07-03 00:36:58 +1000

    koi-syntax: Lexer and Parser public interface

--- crates/koi-syntax/src/lexer.rs
+++ crates/koi-syntax/src/lexer.rs
@@ -1,3 +1,16 @@
+use crate::source::Cursor;
+use crate::token::Token;
+
 pub struct Lexer {
-    
+    cursor: Cursor,
+}
+
+impl Lexer {
+    pub fn with(cursor: Cursor) -> Self {
+        Self { cursor }
+    }
+
+    pub fn next_token(&mut self) -> Option<Token> {
+        todo!()
+    }
 }
--- crates/koi-syntax/src/parser.rs
+++ crates/koi-syntax/src/parser.rs
@@ -1,7 +1,38 @@
+use crate::decl::Decl;
+use crate::expr::Expr;
 use crate::lexer::Lexer;
 use crate::token::*;
+
+pub type Ast = Vec<AstNode>;
+
+pub enum AstNode {
+    Expr(Expr),
+    Decl(Decl),
+}
 
 pub struct Parser {
     lexer: Lexer,
     peeked_token: Option<Token>,
+}
+
+impl Parser {
+    pub fn with(lexer: Lexer) -> Self {
+        Self { lexer, peeked_token: None }
+    }
+
+    pub fn parse(&mut self) -> Ast {
+        let mut nodes = Vec::new();
+
+        while self.lexer.next_token() != None {
+            nodes.push(self.program());
+        }
+
+        nodes
+    }
+}
+
+impl Parser {
+    fn program(&mut self) -> AstNode {
+        todo!()
+    }
 }
