2a8909085dfb1dacbee82590d9a017099b4adce0
tahscenery <tahscenery@gmail.com>
2020-07-03 00:00:21 +1000

    Added `koi-syntax` crate

---
+++ crates/koi-syntax/src/source.rs
@@ -0,0 +1,143 @@
+use std::fs::File;
+use std::fmt::{self, Display};
+use std::io::{self, BufRead, BufReader, Read};
+use std::path::Path;
+use std::vec::IntoIter;
+
+pub const EOF_CHAR: char = '\0';
+
+#[derive(Copy, Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Position {
+    pub line: usize,
+    pub column: usize,
+    offset: usize,
+}
+
+impl Position {
+    pub fn new(line: usize, column: usize, offset: usize) -> Self {
+        Self { line, column, offset }
+    }
+
+    pub fn advance(&mut self) {
+        self.column += 1;
+        self.offset += 1;
+    }
+
+    pub fn advance_line(&mut self) {
+        self.line += 1;
+        self.column = 0;
+        self.offset += 1;
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}:{}]", self.line, self.column)
+    }
+}
+
+pub struct Span {
+    start: Position,
+    end: Position,
+}
+
+#[derive(Copy, Clone)]
+pub enum SourceType {
+    File,
+    Stdin,
+    Stream,
+}
+
+pub struct Source<'a> {
+    pub source_type: SourceType,
+    input: Box<dyn BufRead + 'a>,
+}
+
+impl<'a> Source<'a> {
+    pub fn stdin(stdin: &'a io::Stdin) -> io::Result<Self> {
+        Ok(Self { source_type: SourceType::Stdin, input: Box::new(stdin.lock()) })
+    }
+
+    pub fn file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        File::open(path).map(|file| Self {
+            source_type: SourceType::File,
+            input: Box::new(BufReader::new(file)),
+        })
+    }
+
+    pub fn stream(input: &'a mut dyn BufRead) -> io::Result<Self> {
+        Ok(Self { source_type: SourceType::Stream, input: Box::new(input) })
+    }
+}
+
+impl<'a> Read for Source<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.input.read(buf)
+    }
+}
+
+impl<'a> BufRead for Source<'a> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        self.input.fill_buf()
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.input.consume(amt);
+    }
+}
+
+pub struct Cursor {
+    chars: IntoIter<char>,
+    pub pos: Position,
+}
+
+impl Cursor {
+    pub fn with(mut source: Source) -> Self {
+        let mut buffer = String::new();
+        let chars = match source.read_to_string(&mut buffer) {
+            Ok(bytes) if bytes == 0 => None,
+            Ok(_) => Some(buffer.chars().collect::<Vec<_>>().into_iter()),
+            Err(error) => {
+                eprintln!("Failed to read line: {}", error);
+                None
+            }
+        };
+
+        Self {
+            chars: chars.unwrap_or(Vec::new().into_iter()),
+            pos: Position::default(),
+        }
+    }
+
+    /// Advances to the next character in the `Cursor` iterator.
+    ///
+    /// * If we still have characters left in the line, we'll return the next
+    ///   character in queue.
+    ///
+    /// * If we received `None` (which means we reached the end of the line),
+    ///   then we'll ask our `source` to give us the next line.
+    ///
+    /// * If we are given a new line (and thus the file still has contents in to
+    ///   be processed), then we'll return the next character in our new queue.
+    ///
+    /// * Otherwise, we've reached the end of the file and so we'll return `None`.
+    pub fn advance(&mut self) -> Option<char> {
+        self.chars.next().map(|next_char| {
+            if next_char == '\n' {
+                self.pos.advance_line();
+            } else {
+                self.pos.advance();
+            }
+
+            next_char
+        })
+    }
+
+    pub fn source_len(&self) -> usize {
+        self.chars.len()
+    }
+
+    pub fn nth(&self, n: usize) -> char {
+        self.chars.clone().nth(n).unwrap_or(EOF_CHAR)
+    }
+}
---
+++ crates/koi-syntax/src/token.rs
@@ -0,0 +1,299 @@
+pub struct Token {
+    pub kind: TokenKind,
+}
+
+/// An enum representing all the possible token types.
+#[derive(Clone, Debug, PartialEq)]
+pub enum TokenKind {
+    /// A tag that identifies a variable, type, module, etc.
+    Identifier,
+
+    /// A reserved identifier.
+    Keyword(Keyword),
+
+    /// A literal type represented the same as, or as close to, the Robin
+    /// source code.
+    Literal(Literal),
+
+    /// A character or delimiter with significant meaning of the structure of
+    /// the code.
+    Symbol(Symbol),
+
+    /// A line comment starting with two forward slashes (`//`).
+    LineComment { is_doc_comment: bool, content: Option<String> },
+
+    /// Signifies the end of the current line (if it is still part of the
+    /// current scope).
+    Newline,
+
+    /// Signifies the beginning of a new scope.
+    Begin,
+
+    /// Signifies the end of a scope.
+    End,
+
+    /// An unknown token. An error may be raised if such a token is encountered.
+    Unexpected(char),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Keyword {
+    And,
+    Def,
+    Do,
+    Else,
+    False,
+    If,
+    Let,
+    Match,
+    Module,
+    Not,
+    Or,
+    Public,
+    Then,
+    True,
+    Type,
+    Unimplemented,
+    Using,
+    Val,
+    With,
+}
+
+impl Keyword {
+    pub fn keyword_list() -> Vec<String> {
+        vec![
+            "and",
+            "def",
+            "do",
+            "else",
+            "false",
+            "if",
+            "let",
+            "match",
+            "not",
+            "or",
+            "public",
+            "then",
+            "true",
+            "type",
+            "using",
+            "val",
+            "with",
+        ].into_iter().map(String::from).collect()
+    }
+}
+
+/// Describes the base system used by the number literal encoding.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Base {
+    /// The binary base system (radix = 2). Number literals in binary base start
+    /// with `0b`, for example `0b01`.
+    Binary,
+    /// The octal base system (radix = 8). Number literals in octal base start
+    /// with `0o`, for example `0b07`.
+    Octal,
+    /// The hexadecimal base system (radix = 16). Number literals in hexadecimal
+    /// base start with `0x`, for example `0x0f`.
+    Hexadecimal,
+    /// The decimal base system (radix = 10). This is the default base.
+    Decimal,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Literal {
+    Bool(bool),
+    Char(char),
+    Float { base: Base, value: f64 },
+    Int { base: Base, value: i32 },
+    Str(String),
+    FStr(String),
+    MultiLineStr(Vec<String>),
+}
+
+impl Literal {
+    pub fn description(&self) -> String {
+        match self {
+            Literal::Bool(b) => format!("{}", b),
+            Literal::Char(character) => format!("{}", character),
+            Literal::Float { value, .. } => format!("{}", value),
+            Literal::Int { value, .. } => format!("{}", value),
+            Literal::Str(content) => content.clone(),
+            Literal::FStr(content) => content.clone(),
+            Literal::MultiLineStr(fragments) => fragments.join("\n")
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Symbol {
+    /// The `&` token.
+    Ampersand,
+    /// The `*` token.
+    Asterisk,
+    /// The `@` token.
+    At,
+    /// The `!` token.
+    Bang,
+    /// The `!=` token.
+    BangEq,
+    /// The `^` token.
+    Caret,
+    /// The `:` token.
+    Colon,
+    /// The `,` token.
+    Comma,
+    /// The `$` token.
+    Dollar,
+    /// The `.` token.
+    Dot,
+    /// The `–` token.
+    EnDash,
+    /// The `—` token.
+    EmDash,
+    /// The `=` token.
+    Eq,
+    /// The `-` token.
+    Minus,
+    /// The `%` token.
+    Percent,
+    /// The `+` token.
+    Plus,
+    /// The '#' token.
+    Pound,
+    /// The `?` token.
+    Question,
+    /// The `;` token.
+    Semicolon,
+    /// The `£` token.
+    Sterling,
+    /// The `~` token.
+    Tilde,
+    /// The `|` token.
+    Vertical,
+    /// The `/` token.
+    ForwardSlash,
+    /// The `\` token.
+    BackSlash,
+
+    /// The `<` token.
+    Lt,
+    /// The `<=` token.
+    LtEq,
+    /// The `>` token.
+    Gt,
+    /// The `>=` token.
+    GtEq,
+    /// The `<-` token.
+    LThinArrow,
+    /// The `->` token.
+    RThinArrow,
+    /// The `=>` token.
+    ThickArrow,
+
+    /// The `{` token.
+    LBrace,
+    /// The `}` token.
+    RBrace,
+    /// The `[` token.
+    LBracket,
+    /// The `]` token.
+    RBracket,
+    /// The `(` token.
+    LParen,
+    /// The `)` token.
+    RParen,
+}
+
+impl Symbol {
+    pub fn from_char(c: char) -> Self {
+        use Symbol::*;
+        match c {
+            '&' => Ampersand,
+            '*' => Asterisk,
+            '@' => At,
+            '!' => Bang,
+            '^' => Caret,
+            ':' => Colon,
+            ',' => Comma,
+            '$' => Dollar,
+            '.' => Dot,
+            '–' => EnDash,
+            '—' => EmDash,
+            '=' => Eq,
+            '-' => Minus,
+            '%' => Percent,
+            '+' => Plus,
+            '#' => Pound,
+            '?' => Question,
+            ';' => Semicolon,
+            '£' => Sterling,
+            '~' => Tilde,
+            '|' => Vertical,
+            '/' => ForwardSlash,
+            '\\'=> BackSlash,
+            '<' => Lt,
+            '>' => Gt,
+            '{' => LBrace,
+            '}' => RBrace,
+            '[' => LBracket,
+            ']' => RBracket,
+            '(' => LParen,
+            ')' => RParen,
+            _ => panic!("Character `{}` is not a valid Symbol", c)
+        }
+    }
+
+    pub fn compose(first: char, second: char) -> Option<Self> {
+        match (first, second) {
+            ('!', '=') => Some(Self::BangEq),
+            ('<', '=') => Some(Self::LtEq),
+            ('>', '=') => Some(Self::GtEq),
+            ('<', '-') => Some(Self::LThinArrow),
+            ('-', '>') => Some(Self::RThinArrow),
+            ('=', '>') => Some(Self::ThickArrow),
+            _ => None
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum WhitespaceKind {
+    Space,
+    Tab,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum GroupingDelimiter {
+    Brace,
+    Bracket,
+    Paren,
+}
+
+impl GroupingDelimiter {
+    pub fn string_representation(self) -> String {
+        use GroupingDelimiter::*;
+        match self {
+            Brace => "brace".to_string(),
+            Bracket => "bracket".to_string(),
+            Paren => "parenthesis".to_string(),
+        }
+    }
+
+    pub fn char_representation(self) -> char {
+        use GroupingDelimiter::*;
+        match self {
+            Brace => '}',
+            Bracket => ']',
+            Paren => ')',
+        }
+    }
+
+    pub fn from_char(c: char) -> Self {
+        match c {
+            '{' | '}' => Self::Brace,
+            '[' | ']' => Self::Bracket,
+            '(' | ')' => Self::Paren,
+            _ => panic!("Cannot create a GroupingDelimiter from {:?}", c)
+        }
+    }
+}
