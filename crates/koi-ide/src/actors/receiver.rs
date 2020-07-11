f19553cc2a197e1fcf4e696df9ceef07911647a4
tahscenery <tahscenery@gmail.com>
2020-07-12 01:17:20 +1000

    koi-ide: Minor changes

--- crates/koi-ide/src/actors/receiver.rs
+++ crates/koi-ide/src/actors/receiver.rs
@@ -11,36 +11,33 @@ use std::sync::mpsc::Sender;
 
 pub struct Receiver {
     responder_channel: Sender<LspResponse>,
-    token_database: HashMap<String, (i64, Ast)>,
+    syntax_tree_database: HashMap<String, (i64, Ast)>,
 }
 
 impl Receiver {
     pub fn with(responder_channel: Sender<LspResponse>) -> Self {
-        Receiver { responder_channel, token_database: HashMap::new(), }
+        Receiver { responder_channel, syntax_tree_database: HashMap::new(), }
     }
 
-    fn generate_tokens(&self, path: Url) -> Result<Ast, String> {
+    fn generate_syntax_tree(&self, path: Url) -> Result<Ast, String> {
         match path.to_file_path() {
             Ok(path) => match Source::file(path) {
-                Ok(source) => {
-                    Ok(koi_driver::parse(source))
-                },
+                Ok(source) => Ok(koi_driver::parse(source)),
                 Err(error) => Err(format!("Failed to load file from source: {}", error))
             },
             Err(_) => Err(format!("Failed to convert `{}` to file path.", path))
         }
     }
 
-    fn cache_tokens<V: Into<Option<i64>>>(&mut self, url: Url, version: V) {
+    fn cache_syntax_tree<V: Into<Option<i64>>>(&mut self, url: Url, version: V) {
         let version = version.into().unwrap_or(0);
         let key = url.to_string();
-        match self.token_database.get(&key) {
+        match self.syntax_tree_database.get(&key) {
             Some((cached_version, _)) => {
                 if cached_version != &version {
-                    match self.generate_tokens(url) {
+                    match self.generate_syntax_tree(url) {
                         Ok(tokens) => {
-                            // eprintln!("{:#?}", tokens.nodes());
-                            self.token_database.insert(key, (version, tokens));
+                            self.syntax_tree_database.insert(key, (version, tokens));
                         },
                         Err(error) => {
                             eprintln!("Failed to generate tokens: {}", error);
@@ -49,9 +46,9 @@ impl Receiver {
                 }
             },
             None => {
-                match self.generate_tokens(url) {
+                match self.generate_syntax_tree(url) {
                     Ok(tokens) => {
-                        self.token_database.insert(key, (version, tokens));
+                        self.syntax_tree_database.insert(key, (version, tokens));
                     },
                     Err(error) => {
                         eprintln!("Failed to generate tokens: {}", error)
@@ -62,11 +59,11 @@ impl Receiver {
     }
 
     fn send_hover_response(&self, id: usize, params: lsp_types::TextDocumentPositionParams) {
-        eprintln!("Receiver::send_hover_request")
+        eprintln!("Receiver::send_hover_request");
     }
 
     fn publish_diagnostics(&self, uri: lsp_types::Url) {
-        eprintln!("Receiver::publish_diagnostics")
+        eprintln!("Receiver::publish_diagnostics");
     }
 
     fn process_message(&mut self, message: LspMessage) {
@@ -89,7 +86,7 @@ impl Receiver {
             LspMessage::TextDocumentDidOpenNotification { params, .. } => {
                 let uri = params.text_document.uri;
                 let version = params.text_document.version;
-                self.cache_tokens(uri.clone(), version);
+                self.cache_syntax_tree(uri.clone(), version);
                 self.publish_diagnostics(uri);
             },
             LspMessage::TextDocumentDidChangeNotification { .. } => {
@@ -98,40 +95,35 @@ impl Receiver {
             LspMessage::TextDocumentDidSaveNotification { params } => {
                 let uri = params.text_document.uri;
                 let version = params.text_document.version;
-                self.cache_tokens(uri.clone(), version);
+                self.cache_syntax_tree(uri.clone(), version);
                 self.publish_diagnostics(uri);
             },
             LspMessage::TextDocumentCompletionRequest { id, params } => {
                 use lsp_types::{CompletionItem, CompletionItemKind};
                 use koi_syntax::token::Keyword;
-                let keywords: Vec<CompletionItem> = Keyword::keyword_list()
-                    .into_iter()
-                    .map(|keyword| {
-                        lsp_types::CompletionItem {
-                            label: keyword,
-                            kind: Some(CompletionItemKind::Keyword),
-                            ..CompletionItem::default()
-                        }
-                    })
-                    .collect();
+                let keywords: Vec<CompletionItem> =
+                    Keyword::keyword_list()
+                        .into_iter()
+                        .map(|keyword| {
+                            lsp_types::CompletionItem {
+                                label: keyword,
+                                kind: Some(CompletionItemKind::Keyword),
+                                ..CompletionItem::default()
+                            }
+                        })
+                        .collect();
+
                 let primitive_types: Vec<CompletionItem> =
-                    vec![
-                        "bool",
-                        "char",
-                        "float",
-                        "int",
-                        "uint",
-                        "string",
-                    ]
-                    .into_iter()
-                    .map(|r#type| {
-                        CompletionItem {
-                            label: r#type.to_string(),
-                            kind: Some(lsp_types::CompletionItemKind::Struct),
-                            ..CompletionItem::default()
-                        }
-                    })
-                    .collect();
+                    vec!["bool", "char", "float", "int", "uint", "string"]
+                        .into_iter()
+                        .map(|r#type| {
+                            CompletionItem {
+                                label: r#type.to_string(),
+                                kind: Some(lsp_types::CompletionItemKind::Struct),
+                                ..CompletionItem::default()
+                            }
+                        })
+                        .collect();
 
                 match params.context {
                     Some(context) => match context.trigger_kind {
