c148e9c30a73dc1319ac2cd1d79d354aa3dbaafe
tahscenery <tahscenery@gmail.com>
2020-07-24 19:32:39 +1000

    koi-syntax-old: Appended space to each keyword in list

--- crates/koi-syntax-old/src/token.rs
+++ crates/koi-syntax-old/src/token.rs
@@ -116,9 +116,10 @@ pub enum Keyword {
 impl Keyword {
     pub fn keyword_list() -> Vec<String> {
         vec![
-            "and", "as", "case", "def", "else", "enum", "extend", "external",
-            "if", "internal", "let", "match", "module", "mut", "not", "or",
-            "public", "ref", "return", "struct", "type", "using", "val", "with",
+            "and ", "as ", "case ", "def ", "else ", "enum ", "extend ",
+            "external ", "if ", "internal ", "let ", "match ", "module ", "mut ",
+            "not ", "or ", "public ", "ref ", "return ", "struct ", "type ",
+            "using ", "val ", "with ",
         ].into_iter().map(String::from).collect()
     }
 }
