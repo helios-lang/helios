e8a26364cf4ec9d018edae61d450cc4d3b81f6b0
tahscenery <tahscenery@gmail.com>
2020-11-29 19:33:10 +1100

    Removed old koi-syntax-* crates

--- crates/koi-syntax-old/src/tests/lexer/literals/mod.rs
+++
@@ -1,4 +0,0 @@
-mod integers;
-mod floats;
-
-use super::read_from_string;
--- crates/koi-syntax-old/src/tests/parser/mod.rs
+++
@@ -1,6 +0,0 @@
-#[macro_use] mod macros;
-mod expressions;
-
-fn read_from_string(s: &str) -> &[u8] {
-    s.as_bytes()
-}
