07b4c4544f7ee1b41f7ce28ba1f3ffeff691b600
tahscenery <tahscenery@gmail.com>
2020-12-11 12:56:12 +1100

    koi-repl: Added basic REPL main loop

--- crates/koi-repl/src/lib.rs
+++ crates/koi-repl/src/lib.rs
@@ -1,6 +1,37 @@
 //! REPL support for the Koi programming language.
 
+use std::io::{self, Write};
+
+fn start_main_loop() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut input = String::new();
+
+    println!("Welcome to the REPL. Type :exit to quit.\n");
+
+    loop {
+        write!(stdout, "> ")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+
+        if input.trim() == ":exit" {
+            break;
+        }
+
+        let parse_result = koi_parser::parse(&input);
+        println!("{}", parse_result.debug_tree());
+
+        input.clear();
+    }
+
+    Ok(())
+}
+
 /// Starts a new REPL session.
 pub fn start() {
-    eprintln!("Sorry, REPL support is not available at the moment.");
+    match start_main_loop() {
+        Ok(_) => println!("Goodbye"),
+        Err(error) => eprintln!("An error occurred: {}", error),
+    }
 }
